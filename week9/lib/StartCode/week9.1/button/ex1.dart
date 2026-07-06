af0967af4a7572ce2b6181e05b4d12894286d22d
Kuyseng Marakat <kuysengmarakat13@gmail.com>
2026-07-06 13:01:26 +0700

    week9

--- week9/lib/StartCode/week9.1/button/ex1.dart
+++ week9/lib/StartCode/week9.1/button/ex1.dart
@@ -1,8 +1,34 @@
-import 'dart:convert';
+import 'dart:convert' as convert;
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+class ButtonStatus {
+  ButtonStatus({required this.title, required this.selected});
+
+  final String title;
+  final bool selected;
+}
+
+enum AsyncState { loading, success, error }
+
+class AsyncData<V> {
+  final V? value;
+  final String? error;
+  final AsyncState state;
+
+  AsyncData.loading() : value = null, error = null, state = AsyncState.loading;
+
+  AsyncData.success(this.value) : error = null, state = AsyncState.success;
+
+  AsyncData.error(this.error) : value = null, state = AsyncState.error;
+}
+
+class RepositoryException implements Exception {
+  final String message;
+
+  RepositoryException(this.message);
+}
 
 class MyButton extends StatefulWidget {
   const MyButton({super.key});
@@ -14,16 +40,22 @@ class MyButton extends StatefulWidget {
 class _MyButtonState extends State<MyButton> {
   bool isSelected = false;
 
+  AsyncData<ButtonStatus> data = AsyncData.loading();
+
   final String url =
       "https://mobile-fc8af-default-rtdb.asia-southeast1.firebasedatabase.app/.json";
 
-  AsyncData<ButtonStatus> data = AsyncData.loading();
+  void selected() {
+    setState(() {
+      isSelected = !isSelected;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
 
-    // Fetch init state from Firebase
+    // Fetch init state
     _fetchButtonData();
   }
 
@@ -32,7 +64,6 @@ class _MyButtonState extends State<MyButton> {
 
     try {
       ButtonStatus buttonStatus = await getButtonStatus();
-
       setState(() {
         data = AsyncData.success(buttonStatus);
         isSelected = buttonStatus.selected;
@@ -41,144 +72,46 @@ class _MyButtonState extends State<MyButton> {
       setState(() => data = AsyncData.error(e.message));
     }
   }
+
   Future<ButtonStatus> getButtonStatus() async {
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode != 200) {
-        throw RepositoryException(
-          "Failed to fetch data from Firebase",
-        );
-      }
+    Uri uri = Uri.parse(url);
 
+    http.Response response = await http.get(uri);
 
-      final decodedData = jsonDecode(response.body);
+    Map<String, dynamic> json =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
 
-      if (decodedData == null) {
-        throw RepositoryException("Firebase data is empty");
-      }
+    String title = json['title'] as String;
+    bool selected = json['selected'] as bool;
 
-      if (decodedData is! Map<String, dynamic>) {
-        throw RepositoryException("Firebase data structure is wrong");
-      }
-
-      return ButtonStatus.fromJson(decodedData);
-    } catch (e) {
-      if (e is RepositoryException) {
-        throw e;
-      }
-
-      throw RepositoryException("No internet connection or Firebase error");
-    }
+    return ButtonStatus(title: title, selected: selected);
   }
 
-  void selected() {
-    setState(() {
-      isSelected = !isSelected;
-    });
-  }
   @override
   Widget build(BuildContext context) {
+    Widget content;
+
     if (data.state == AsyncState.loading) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
+      content = const CircularProgressIndicator();
+    } else if (data.state == AsyncState.error) {
+      content = Text(data.error ?? "Something is wrong!");
+    } else {
+      content = ElevatedButton(
+        onPressed: selected,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isSelected ? Colors.lightBlueAccent : Colors.white,
         ),
+        child: Text(isSelected ? "Selected" : "Not Selected"),
       );
     }
-
-    if (data.state == AsyncState.error) {
-      return Scaffold(
-        body: Center(
-          child: Text(
-            data.error ?? "Unknown error",
-            style: TextStyle(color: Colors.red),
-          ),
-        ),
-      );
-    }
-
-    final ButtonStatus buttonStatus = data.value!;
-
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: selected,
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size(350, 70),
-            backgroundColor: isSelected
-                ? const Color.fromARGB(255, 97, 158, 187)
-                : Colors.white,
-            foregroundColor: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(40),
-              side: const BorderSide(color: Colors.grey),
-            ),
-          ),
-          child: Text(
-            buttonStatus.name.toUpperCase(),
-            style: const TextStyle(fontSize: 22),
-          ),
+        body: Center(
+          child: content,
         ),
-      ),
-    );
+      );
   }
 }
 
-class ButtonStatus {
-  final String name;
-  final bool selected;
-
-  ButtonStatus({
-    required this.name,
-    required this.selected,
-  });
-
-  factory ButtonStatus.fromJson(Map<String, dynamic> json) {
-    if (json["name"] is! String) {
-      throw RepositoryException("Field 'name' is missing or wrong");
-    }
-
-    if (json["selected"] is! bool) {
-      throw RepositoryException("Field 'selected' is missing or wrong");
-    }
-    return ButtonStatus(
-      name: json["name"],
-      selected: json["selected"],
-    );
-  }
-}
-
-enum AsyncState {
-  loading,
-  success,
-  error,
-}
-
-class AsyncData<T> {
-  final T? value;
-  final String? error;
-  final AsyncState state;
-
-  AsyncData.loading()
-      : value = null,
-        error = null,
-        state = AsyncState.loading;
-  AsyncData.success(this.value)
-      : error = null,
-        state = AsyncState.success;
-
-  AsyncData.error(this.error)
-      : value = null,
-        state = AsyncState.error;
-}
-class RepositoryException implements Exception {
-  final String message;
-
-  RepositoryException(this.message);
-}
 void main() {
-  runApp(MaterialApp(
-    home: MyButton(),
-  ));
+  runApp(MaterialApp(home: MyButton()));
 }
-
